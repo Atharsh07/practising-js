f4c4867b0c4c19cda831114985def9380e1524b3
atharsh07 <atharsh0425@gmail.com>
2024-10-13 16:07:37 +0530

    todo done

--- dom/script.js
+++ dom/script.js
@@ -1,5 +1,18 @@
+let id = 1;
+function todolist() {
+    const readele = document.querySelector("input");
+    const value = readele.value;
 
-function deletetodo(index) {
-    const element = document.getElementById("todo-" + index);
+    const newdiv = document.createElement("div");
+    newdiv.setAttribute("id", id);
+    id = id + 1; // increment after setting the id
+    newdiv.innerHTML = `<div>${value} <button onclick="deletetoto(${id - 1})">delete</button> </div>`;
+
+    document.querySelector("body").appendChild(newdiv);
+}
+
+
+function deletetoto(index) {
+    const element = document.getElementById(index)
     element.parentNode.removeChild(element);
 }
