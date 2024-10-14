007fdcc6994597d00dc4e262ce1e630641e15516
atharsh07 <atharsh0425@gmail.com>
2024-10-14 14:11:46 +0530

    first commit

---
+++ node.js/index.js
@@ -0,0 +1,25 @@
+// middlewarae concept
+
+const express = require('express')
+const app = express()
+
+function  agecheckmiddleware(req, res, next) {
+    const age = req.query.age;
+    if(age>=14){
+        next();
+    }else{
+        res.send("your under age no-entry");
+    }
+}
+
+app.use(agecheckmiddleware);
+app.get('/ride1', function(req, res) {
+    res.send("you are welcome to the ride1")
+})
+app.get('/ride2', function(req, res) {
+    res.send("your are welcome to the ride2")
+})
+
+app.listen(3000);
+
+//
