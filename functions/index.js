162edf8d2e9328655bfde51ced041bb5a5d7c057
atharsh07 <atharsh0425@gmail.com>
2024-10-15 12:01:15 +0530

    func-arr

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 // map, fiters, arrow func
 
-// const sum = (a,b) => {
-//     return a + b ;
-// }
-// console.log(sum(1, 3))
+const sum = (a,b) => {
+    return a + b ;
+}
+console.log(sum(1, 3))
 
 
 const arr = [1,2,3,4,5,6]
@@ -13,3 +13,14 @@ const arr = [1,2,3,4,5,6]
 
  const ans = arr.map(multiply)
 console.log(ans);
+
+const arr = [1,2,3,4,5,6,7,8,9]
+
+const ans = arr.filter((n)=>{
+    if(n%2 == 0){
+        return true
+    }else{
+        return false
+    }
+})
+console.log(ans)
